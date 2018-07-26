5656b093398c8ab49689279a496e270b3bf4ad69
sushant-wadhone-mpf <sushant.wadhone@synechron.com>
2018-07-26 09:27:39 +0530

    Basic Spring web MVC

---
+++ src/test/java/com/wadhone/mvc/PracticeMVC/PracticeMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wadhone.mvc.PracticeMVC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PracticeMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
